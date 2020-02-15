f48c2785b8d98f8e0bcc490e9ef4b21c3f1a2f27
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-15 11:13:05 +0000

    Initial
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ docs/precache-manifest.bfcc4b08231e7d33cb20465fd029d861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c3b478b8218f41587ca0a471a64d3b",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "c99e465219ef01b7d432",
+    "url": "/docs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d1fb2aad0cf371152d4",
+    "url": "/docs/static/js/2.2548e74d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/docs/static/js/2.2548e74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99e465219ef01b7d432",
+    "url": "/docs/static/js/main.8a5d36f4.chunk.js"
+  },
+  {
+    "revision": "0b0a6d305219fe69af3e",
+    "url": "/docs/static/js/runtime-main.9cf22372.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a8ad0c0c4c3157a7bba423f100bb162.js"
+  "/docs/precache-manifest.bfcc4b08231e7d33cb20465fd029d861.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/docs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
