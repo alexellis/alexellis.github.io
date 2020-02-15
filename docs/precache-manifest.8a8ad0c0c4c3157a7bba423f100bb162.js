0bdb5119d9f9e621e5f9add8327039e874b1e0f2
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-15 11:06:42 +0000

    Initial
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ docs/precache-manifest.8a8ad0c0c4c3157a7bba423f100bb162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cf8c3ddf70a5aa25f11afb18d799a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4563e16b7197fdd4962",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d1fb2aad0cf371152d4",
+    "url": "/static/js/2.2548e74d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2548e74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4563e16b7197fdd4962",
+    "url": "/static/js/main.59c31f88.chunk.js"
+  },
+  {
+    "revision": "75e1fe586a99595f90c4",
+    "url": "/static/js/runtime-main.3d1e4360.js"
+  }
+]);
